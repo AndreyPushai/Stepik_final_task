6f1499fc741d335c11bca37fc1ca2fa0efc20cb4
AndreyPushai <andrey.pushai@gmail.com>
2020-07-21 13:33:54 +0300

    Requirements for environment, init file, conftest and test_main_page added

---
+++ conftest.py
@@ -0,0 +1,36 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+supported_browsers = {
+        'chrome' : webdriver.Chrome,
+        'firefox' : webdriver.Firefox
+        }
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome', help="You should enter --browser_name={name of the browser}")
+    parser.addoption('--language', action='store', default='en', help="You should enter --language={test language}")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption('browser_name')
+    language = request.config.getoption('language')
+
+    if browser_name == 'chrome':
+        print(f"\n Starting {browser_name} browser for test...")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+
+    elif browser_name == 'firefox':
+        print(f"\n Starting {browser_name} browser for test...")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+        
+    else:
+        raise pytest.UsageError(f"--browser_name is invalid, supported browsers: chrome, firefox")
+
+    yield browser
+    print("\n Quiting browser...")
+    browser.quit()
