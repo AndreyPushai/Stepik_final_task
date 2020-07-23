47218c3df1f7e30c0ff58dbdbad67d57b382ebe8
AndreyPushai <andrey.pushai@gmail.com>
2020-07-23 13:18:16 +0300

    Added headless modes for Chrome and Firefox browsers

--- conftest.py
+++ conftest.py
@@ -1,7 +1,8 @@
 import pytest
 #import time
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 supported_browsers = {
         'chrome' : webdriver.Chrome,
@@ -19,14 +20,18 @@ def browser(request):
 
     if browser_name == 'chrome':
         print(f"\n Starting {browser_name} browser for test...")
-        options = Options()
+        options = ChromeOptions()
         options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        # options.add_argument("--headless")
         browser = webdriver.Chrome(options=options)
 
     elif browser_name == 'firefox':
         print(f"\n Starting {browser_name} browser for test...")
         fp = webdriver.FirefoxProfile()
         fp.set_preference("intl.accept_languages", language)
+        # options = FirefoxOptions()
+        # options.headless = True
+        # browser = webdriver.Firefox(options=options, firefox_profile=fp)
         browser = webdriver.Firefox(firefox_profile=fp)
         
     else:
