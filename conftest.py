a0a201f0c7f15870a8580956855940548ee6ce49
AndreyPushai <andrey.pushai@gmail.com>
2020-07-28 14:57:22 +0300

    Removed mocked headless and full screen options, also removed time.sleep

--- conftest.py
+++ conftest.py
@@ -1,5 +1,4 @@
 import pytest
-#import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
@@ -22,23 +21,17 @@ def browser(request):
         print(f"\n Starting {browser_name} browser for test...")
         options = ChromeOptions()
         options.add_experimental_option('prefs', {'intl.accept_languages': language})
-        # options.add_argument("--headless")
-        # options.add_argument("--start-maximized")
         browser = webdriver.Chrome(options=options)
 
     elif browser_name == 'firefox':
         print(f"\n Starting {browser_name} browser for test...")
         fp = webdriver.FirefoxProfile()
         fp.set_preference("intl.accept_languages", language)
-        # options = FirefoxOptions()
-        # options.headless = True
-        # browser = webdriver.Firefox(options=options, firefox_profile=fp)
         browser = webdriver.Firefox(firefox_profile=fp)
         
     else:
         raise pytest.UsageError(f"--browser_name is invalid, supported browsers: chrome, firefox")
 
     yield browser
-    #time.sleep(3)
     print("\n Quiting browser...")
     browser.quit()
